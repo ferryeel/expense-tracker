b991c4cc7308dff43a39c4787262a40e0619e79b
ferryeel <feriel.nouri@sesame.com.tn>
2026-03-04 10:55:31 +0100

    fix: remove middleware in favor of client-side route protection

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from '@clerk/nextjs';
-
-export default authMiddleware({
-  publicRoutes: ['/', '/sign-in', '/sign-up', '/api/auth/(.*)'],
-});
-
-export const config = {
-  matcher: ['/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest))(?:.*))'],
-};
